a233de7dd8a655ac7f011183539fa989609ef7ba
ishtiakahamad <ishtiakahamed2019@gmail.com>
2026-07-30 13:42:22 +0600

    Update homework8.py

--- lec08/homework8.py
+++ lec08/homework8.py
@@ -1,45 +1,32 @@
 import numpy as np
 
 def waveform_to_frames(waveform, frame_length, step):
-    '''
-    Chop a waveform into overlapping frames.
+   
+    N = len(waveform)
     
-    @params:
-    waveform (np.ndarray(N)) - the waveform
-    frame_length (scalar) - length of the frame, in samples
-    step (scalar) - step size, in samples
+    num_frames = 1 + (N - frame_length) 
     
-    @returns:
-    frames (np.ndarray((num_frames, frame_length))) - waveform chopped into frames
-       frames[m/step,n] = waveform[m+n] only for m = integer multiple of step
-    '''
-    raise RuntimeError("You need to change this part")
+    frames = np.zeros((num_frames, frame_length))
+    
+    for i in range(num_frames):
+        
+        m = i * step
+        frames[i, :] = waveform[m:m + frame_length]
+        
+    return frames
+
 
 def frames_to_mstft(frames):
-    '''
-    Take the magnitude FFT of every row of the frames matrix.
+   
+    mstft = np.abs(np.fft.fft(frames, axis=1))
     
-    @params:
-    frames (np.ndarray((num_frames, frame_length))) - the speech samples
-    
-    @returns:
-    mstft (np.ndarray((num_frames, frame_length))) - the magnitude short-time Fourier transform
-    '''
-    raise RuntimeError("You need to change this part")
+    return mstft
+
 
 def mstft_to_spectrogram(mstft):
-    '''
-    Convert max(0.001*amax(mstft), mstft) to decibels.
+   
+    threshold = 0.001 * np.amax(mstft)
+    clipped = np.maximum(threshold, mstft)
+    spectrogram = 20 * np.log10(clipped)
     
-    @params:
-    stft (np.ndarray((num_frames, frame_length))) - magnitude short-time Fourier transform
-    
-    @returns:
-    spectrogram (np.ndarray((num_frames, frame_length)) - spectrogram 
-    
-    The spectrogram should be expressed in decibels (20*log10(mstft)).
-    np.amin(spectrogram) should be no smaller than np.amax(spectrogram)-60
-    '''
-    raise RuntimeError("You need to change this part")
-
-
+    return spectrogram
